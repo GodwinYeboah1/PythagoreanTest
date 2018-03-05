ffc28abb6570137ff1b57ec12c775c96c9d17beb
Godwin Yeboah <godwinyeboah95@gmail.com>
2018-03-05 11:28:21 -0500

    Add files via upload

---
+++ PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest{
+public static void main(String[] args) {
+	Pythagorean	hyp = new Pythagorean();
+	double result =  hyp.calculateHypotenuse(5, 6);
+	System.out.println(result);
+	}
+}
